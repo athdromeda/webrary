01566c8320f1c6d53aac577739b36170a06c8135
athdromeda <anixatho01@gmail.com>
2021-12-28 09:44:31 +0700

    extract widget ItemCard and refining UI

---
+++ lib/ui/item.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class ItemCard extends StatelessWidget {
+  String title;
+  String subtitle;
+  ItemCard({Key? key, required this.title, required this.subtitle})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Slidable(
+      key: ValueKey(0),
+      child: ListTile(
+        title: Text(
+          title,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        subtitle: Text(subtitle),
+      ),
+      endActionPane: ActionPane(
+        motion: BehindMotion(),
+        children: [
+          SlidableAction(
+            onPressed: null,
+            icon: Icons.delete,
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+            label: 'Hapus',
+          )
+        ],
+      ),
+    );
+  }
+}
